ba9a816c9abb286215cb14722cd6ac742d377288
skrealworld <sk1846@rit.edu>
2015-07-07 22:17:28 -0400

    file1 same CNN used at CVPR 2015 torch7 tutorial

--- vid_mat.m
+++ vid_mat.m
@@ -1,14 +1,13 @@
 clc;
-clear all;
-DBpath='C:\Users\sk1846\Desktop\Matconvnet\Small UCF101_20\'; % DataBase path 
-load('ClassLabels.mat') % Load the labeled mat file for classNames.
+DBpath='~/skdata/UCF-101/'; % DataBase path 
+load('~/skdata/ClassLabels.mat') % Load the labeled mat file for classNames.
 count_smpl=1;
 count_idx=0;
 label=1;
-noOfSamp1Vid=35;    % Define the number if training videos from each class. 
-for f1=20     % Define the number of classes.
-    for f2=1:noOfSamp1Vid   
-        obj=VideoReader(strcat(DBpath,classNames{f1},'\',classNames{f1},' (',num2str(f2),').avi')); %Access the video.
+noOfSamp1Vid=5;    % Define the number if training videos from each class. 
+for f1=1:2     % Define the number of classes.
+    for f2=1:noOfSamp1Vid
+        obj=VideoReader(strcat(DBpath,classNames{f1},'/',classNames{f1},' (',num2str(f2),').avi')); %Access the video.
             nFrames=obj.NumberOfFrames; % Get the number of frames. 
             n10=1; 
             for n1=5:20:nFrames   
@@ -20,19 +19,18 @@ for f1=20     % Define the number of classes.
                     end
                     idx=(180*count_idx)+1;
                     im=read(obj,n2);   % Read the selected frame. 
-                    im=imresize(im,[60,80]) % Change the resolution from 240x320 to 60x80. 
+                    im=imresize(im,[60,80]); % Change the resolution from 240x320 to 60x80. 
                     frame(idx:idx+60-1,:,:)=im(:,:,1);          % Stacking all three(R,G,B) components of image. 
                     frame(idx+60:idx+120-1,:,:)=im(:,:,2);
                     frame(idx+120:idx+180-1,:,:)=im(:,:,3);
                     count_idx=count_idx+1;
                     vid_data(:,:,t_dim,count_smpl)=frame(idx:idx+180-1,:);
-                    clear frame;
                 end
                 Y(count_smpl)=label;
                 count_smpl=count_smpl+1;
                 n10=n10+1;
             end
-            sprintf('Number of samples of %d th video of class %d is %d',f2,f1,n1)
+            sprintf('Number of samples of %d th video of class %d is %d',f2,f1,n10)
     end
     lable=label+1;
 end
