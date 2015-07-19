5f130aff548e42ea691f7597a288a0f46fa7bbfb
skrealworld <sk1846@rit.edu>
2015-07-18 23:44:57 -0400

    Sinteractive version of video to .mat file

--- vid_mat.m
+++ vid_mat.m
@@ -1,30 +1,34 @@
 clc;
-DBpath='~/skdata/UCF-101/'; % DataBase path 
-load('~/skdata/ClassLabels.mat') % Load the labeled mat file for classNames.
+load('ClassLabels.mat') % Load the labeled mat file for classNames.
 count_smpl=1;
 count_idx=0;
 label=1;
-noOfSamp1Vid=5;    % Define the number if training videos from each class. 
-for f1=1:2     % Define the number of classes.
-    for f2=1:noOfSamp1Vid
-        obj=VideoReader(strcat(DBpath,classNames{f1},'/',classNames{f1},' (',num2str(f2),').avi')); %Access the video.
+for f1=1:20     % Define the number of classes.
+    cd (strcat('~/skdata/UCF-101/',classNames{f1})); % DataBase path
+    d =dir; 
+    num_of_videos =length(d);
+
+    for f2=1:num_of_videos   
+        obj=VideoReader(strcat(classNames{f1},' (',num2str(f2),').ogv')); %Access the video.
             nFrames=obj.NumberOfFrames; % Get the number of frames. 
-            n10=1; 
-            for n1=5:20:nFrames   
+            n10=1; %????
+            for n1=8:20:nFrames   
                 t_dim=0;
-                for n2=n1-4:n1+5
+                for n2=n1-7:n1+7
                     t_dim=t_dim+1;
                     if(n2>=nFrames)    
-                        break;
+                        break
                     end
-                    idx=(180*count_idx)+1;
+                    %idx=(180*count_idx)+1;
                     im=read(obj,n2);   % Read the selected frame. 
-                    im=imresize(im,[60,80]); % Change the resolution from 240x320 to 60x80. 
-                    frame(idx:idx+60-1,:,:)=im(:,:,1);          % Stacking all three(R,G,B) components of image. 
-                    frame(idx+60:idx+120-1,:,:)=im(:,:,2);
-                    frame(idx+120:idx+180-1,:,:)=im(:,:,3);
+                    im=imresize(im,[64,64]); % Change the resolution from 240x320 to 64x84. 
+                    im_temp= rgb2ycbcr(im);
+                    im = im_temp(:,:,1);
+                    %frame(idx:idx+60-1,:,:)=im(:,:,1);          % Stacking all three(R,G,B) components of image. 
+                    %frame(idx+60:idx+120-1,:,:)=im(:,:,2);
+                    %frame(idx+120:idx+180-1,:,:)=im(:,:,3);
                     count_idx=count_idx+1;
-                    vid_data(:,:,t_dim,count_smpl)=frame(idx:idx+180-1,:);
+                    vid_data(:,:,t_dim,count_smpl)=im;
                 end
                 Y(count_smpl)=label;
                 count_smpl=count_smpl+1;
@@ -32,7 +36,10 @@ for f1=1:2     % Define the number of classes.
             end
             sprintf('Number of samples of %d th video of class %d is %d',f2,f1,n10)
     end
-    lable=label+1;
+    label=label+1;
 end
-save('vid_data_training.mat','vid_data');
-save('vid_data_training_labels.mat','Y');
+%tt= vid_data(:,:,:,1:707);
+%yy= 6+Y(1:707);
+cd ('~/matlab_work');
+save('vid_data_train_20.mat','vid_data');
+save('vid_data_trainlabels1_20.mat','Y');
