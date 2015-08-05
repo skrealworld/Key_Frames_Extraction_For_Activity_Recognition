36c3c3fcfdcce3f7342e1d0bf638af4782e249c8
Sourabh Kulhare <skrealworld@users.noreply.github.com>
2015-08-05 14:17:46 -0400

    Working with random samples.

--- Script.lua
+++ Script.lua
@@ -1,6 +1,7 @@
 package.path = package.path .. ';Models/?.lua'  --define path for Models
 
-model = require 'Alexnet_eladhoffer'    -- Select the model      
+model = require 'Alexnet_eladhoffer'    -- Select the model
+model = model:cuda()
 print(model)
 
 require 'mattorch'
@@ -12,26 +13,36 @@ require 'mattorch'
 --local testx  = mattorch.load('testX.mat')
 ----local testy  = mattorch.load('testY.mat')
 
+
 trainset = {}
-trainset.data = torch.rand(20000,15,64,64):cuda()
-trainset.label = torch.rand(20000,1):cuda()
+trainset.data = torch.rand(200,15,64,64):cuda()
+temp1 = torch.rand(200)
+for i=1,200 do temp1[i] = math.random(1,101) end
+trainset.label = torch.rand(200,1)
+trainset.label[{{},1}] = temp1
+trainset.label = trainset.label:cuda()
 --trainset.data = trainx.train:cuda()
 --trainset.label = trainy.trainY:cuda()
 print(trainset)
 testset = {}
-testset.data = torch.rand(5000,15,64,64):cuda()
-testset.label = torch.rand(5000,1):cuda()
+testset.data = torch.rand(50,15,64,64):cuda()
+temp2 = torch.rand(50)
+for i=1,50 do temp1[i] = math.random(1,101) end
+testset.label = torch.rand(50,1)
+testset.label[{{},1}] = temp2
+testset.label = testset.label:cuda()
 --testset.data = testx.vid_data:cuda()
 --testset.data = testy.Y:cuda()
 print(testset)
+
+setmetatable(trainset,
+    {__index = function(t, i)
+                    return {t.data[i], t.label[i]}
+                end}
+);
+
 criterion  = nn.ClassNLLCriterion()
 criterion = criterion:cuda()
-
-trainer = nn.StochasticGradient(model,criterion)
-trainer.learningRate = 0.001
-trainer.maxIteration = 2
-
-trainer:train(trainset)
-
-predicted = model:forward(testset.data[50])
-print(predicted)
+function trainset:size()
+        return self.data:size(1)
+end
